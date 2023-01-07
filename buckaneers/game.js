7d873b421cb7455bb0763a3903eb016b167c9470
Cedric Hutchings <ced@misguided.enterprises>
2023-01-07 14:20:55 -0600

    hands

--- buckaneers/game.js
+++ buckaneers/game.js
@@ -8,6 +8,18 @@ const SECOND_IN_TICKS = 60;
 
 /* vektorr maffz */
 function lerp(v0, v1, t) { return (1 - t) * v0 + t * v1; }
+function lerp_rads(a, b, t) {
+  const fmodf = (l, r) => l % r;
+  const difference = fmodf(b - a, Math.PI*2.0),
+        distance = fmodf(2.0 * difference, Math.PI*2.0) - difference;
+  return a + distance * t;
+}
+function rad_distance(a, b) {
+  const fmodf = (l, r) => l % r;
+  const difference = fmodf(b - a, Math.PI*2.0),
+        distance = fmodf(2.0 * difference, Math.PI*2.0) - difference;
+  return distance;
+}
 function mag(x, y) { return Math.sqrt(x*x + y*y); }
 function norm(obj) {
   const m = mag(obj.x, obj.y);
@@ -167,10 +179,9 @@ if ((BROWSER && BROWSER_HOST) || NODE) {
 
       /* discard the fields we don't want to send them */
       const { tick } = state;
-      const particles = state.particles.map(({ id, x, y, death_tick }) => {
-        const ttl = death_tick - state.tick;
-        return { id, x, y, ttl };
-      });
+      const particles = state
+        .particles
+        .map(({ id, x, y, death_tick }) => ({ id, x, y, death_tick }));
       const players = Object
         .values(state.players)
         .map(({ id, x, y }) => ({ id, x, y }));
@@ -282,7 +293,20 @@ if (NODE) {
     ws.on('message', (data) => send_host(id, data.toString()));
   }
 
-  setInterval(() => {
+  /* setInterval is known to drift and be sloppy
+   * this eats more CPU cycles, but it gives a more reliable tick */
+  const setTick = (ms, logic) => {
+    const start = performance.now();
+    let tick = 0;
+    (function step() {
+      setTimeout(step, 0);
+
+      while ((performance.now() - start) / ms > tick)
+        logic(tick++);
+    })();
+  }
+
+  setTick(1000/60, () => {
     host_tick();
 
     let msg;
@@ -291,7 +315,7 @@ if (NODE) {
         if (clients[id].readyState === ws.OPEN)
           clients[id].send(msg);
       }
-  }, 1000/50);
+  });
 }
 
 /* default client state */
@@ -340,7 +364,9 @@ if (BROWSER_HOST) {
   }
   dev_cache_state = state => LS.setItem(key, JSON.stringify(state));
 }
-if (BROWSER) window.onload = function frame() {
+
+let last, host_tick_accumulator = 0;
+if (BROWSER) window.onload = function frame(elapsed) {
   requestAnimationFrame(frame);
 
   const state = client_state();
@@ -355,8 +381,8 @@ if (BROWSER) window.onload = function frame() {
     p2.style.bottom = '0px';
     p2.style.left = '0px';
 
-    client(p1, state.p1);
-    client(p2, state.p2);
+    client(p1, elapsed, state.p1);
+    client(p2, elapsed, state.p2);
 
     window.onmousedown = e => {
       const state = client_state();
@@ -380,14 +406,32 @@ if (BROWSER) window.onload = function frame() {
     const p1 = document.getElementById("player1");
     p1.width  = window.innerWidth;
     p1.height = window.innerHeight;
-    client(p1, state.p1);
+    client(p1, elapsed, state.p1);
   }
   dev_cache_state(state);
 
-  host_tick();
+  if (BROWSER_HOST) {
+    /* we want to make sure there's always 1 tick every 60hz,
+     * even if e.g. the renderer is lagging.
+     *
+     * (this makes the speed of objects uniform) */
+
+    if (isFinite(elapsed) && isFinite(last))
+      host_tick_accumulator += elapsed - last;
+    last = elapsed;
+
+    while (host_tick_accumulator > 1000/60) {
+      host_tick_accumulator -= 1000/60;
+      host_tick();
+    }
+  }
+  else
+    /* it's just used for sending websockets messages,
+     * doesn't really matter how often we call it */
+    host_tick();
 }
 
-function client(canvas, state) {
+function client(canvas, elapsed, state) {
   const id = state.id;
 
   /* send messages to server */
@@ -429,12 +473,16 @@ function client(canvas, state) {
   send_host(id, JSON.stringify(["move", norm(move)]));
 
 
-  render(canvas, state);
+  render(canvas, elapsed, state);
 }
 
 const spritesheet = new Image();
 spritesheet.src = "art.png";
-function render(canvas, state) {
+function render(canvas, elapsed, state) {
+  let dt = 0;
+  if (state.last_render != undefined) dt = elapsed - state.last_render;
+  state.last_render = elapsed;
+
   const { world, last_world, cam } = state;
 
   /* initialize canvas */
@@ -468,12 +516,19 @@ function render(canvas, state) {
     ctx.fillStyle = "snow";
     ctx.fillRect(0, 0, 1, 1);
 
-    const TILE_PLAYER  = { w: 1, h: 1, x: 0, y: 8 };
-    const TILE_PLAYER0 = { w: 1, h: 1, x: 2, y: 8 };
+    /* idk, let's have 22 tiles on the screen? */
+    const TILE_SIZE = 1/22;
+
+    const TILE_PLAYERS = [
+      { body: { w: 1, h: 1, x: 0, y: 8 },
+        hand: { w: 1, h: 1, x: 1, y: 8 } },
+      { body: { w: 1, h: 1, x: 2, y: 8 },
+        hand: { w: 1, h: 1, x: 3, y: 8 } },
+    ];
     const TILE_SPEAR   = { w: 1, h: 1, x: 4, y: 9 };
 
     const draw_tile = (tile, dx, dy, dsize, angle) => {
-      const TILE_SIZE = spritesheet.height/11;
+      const TILE_PIXELS = spritesheet.height/11;
 
       if (angle != undefined) {
         ctx.save();
@@ -482,14 +537,14 @@ function render(canvas, state) {
         ctx.drawImage(
           spritesheet,
 
-          /* source x & y */
-          tile.x*TILE_SIZE, tile.y*TILE_SIZE,
-          /* source width and height */
-          tile.w*TILE_SIZE, tile.h*TILE_SIZE,
+          /* source x & y on spritesheet */
+          tile.x*TILE_PIXELS, tile.y*TILE_PIXELS,
+          /* source width and height on spritesheet */
+          tile.w*TILE_PIXELS, tile.h*TILE_PIXELS,
 
-          /* destination x & y */
+          /* destination x & y in world */
           - dsize/2, - dsize/2,
-          /* destination width and height */
+          /* destination width and height in world */
           dsize, dsize
         );
         ctx.restore();
@@ -499,36 +554,71 @@ function render(canvas, state) {
       ctx.drawImage(
         spritesheet,
 
-        /* source x & y */
-        tile.x*TILE_SIZE, tile.y*TILE_SIZE,
-        /* source width and height */
-        tile.w*TILE_SIZE, tile.h*TILE_SIZE,
+        /* source x & y on spritesheet */
+        tile.x*TILE_PIXELS, tile.y*TILE_PIXELS,
+        /* source width and height on spritesheet */
+        tile.w*TILE_PIXELS, tile.h*TILE_PIXELS,
 
-        /* destination x & y */
+        /* destination x & y in world */
         dx - dsize/2, dy - dsize/2,
-        /* destination width and height */
+        /* destination width and height in world */
         dsize, dsize
       );
     }
 
-    for (const { id, x, y } of world.players) {
-      const size = 0.05;
+    for (const p of world.players) {
+      const { id, x, y } = p;
 
-      const tile = id == world.you ? TILE_PLAYER0 : TILE_PLAYER;
-      draw_tile(tile, x, y, size);
+      const { body, hand } = TILE_PLAYERS[(id == world.you) ? 1 : 0];
+
+      const last_pos = last_world.players.find(x => x.id == id);
+      if (!last_pos) continue;
+      const d = { x: x - last_pos.x, y: y - last_pos.y };
+
+      if (last_pos.angle == undefined) last_pos.angle = 0;
+      if (last_pos.damp  == undefined) last_pos.damp  = 0;
+
+      const ideal_damp = (mag(d.x, d.y) > 0) ? 1 : 0;
+      const delta = ideal_damp - last_pos.damp;
+      last_pos.damp += Math.sign(delta) * Math.min(Math.abs(delta), dt*0.004);
+      const damp = last_pos.damp;
+      p.damp = damp;
+
+      /* intentional, want perp (also fuck atan2's function signature) */
+      const ideal_angle = (mag(d.x, d.y) > 0)
+        ? Math.atan2( -d.x , d.y)
+        : last_pos.angle;
+
+      const distance = rad_distance(last_pos.angle, ideal_angle);
+      const force = Math.min(dt*0.007, Math.abs(distance));
+      let angle = last_pos.angle + force*Math.sign(distance);
+      p.angle = angle;
+
+      const jog     = Math.cos(elapsed*0.01    )*0.2*damp;
+      const headjog = Math.cos(elapsed*0.01*0.5)*0.2*damp;
+      angle += jog;
+
+      const hand_space = TILE_SIZE * 0.58;
+      const ox = hand_space*Math.cos(angle) + d.x*jog*8;
+      const oy = hand_space*Math.sin(angle) + d.y*jog*8;
+
+      draw_tile(body, x + d.x*headjog*3,
+                      y + d.y*headjog*3, TILE_SIZE);
+      draw_tile(hand, x + ox,
+                      y + oy, TILE_SIZE);
+      draw_tile(hand, x - ox,
+                      y - oy, TILE_SIZE);
     }
     for (const { x, y, death_tick, id } of world.particles) {
-      const size = 0.05;
-
       /* canvas treats alphas > 1 the same as 1 */
       const ttl = death_tick - world.tick;
-      ctx.globalAlpha = ttl / SECOND_IN_TICKS;
+      ctx.globalAlpha = ttl / SECOND_IN_TICKS/2;
 
       const last_pos = last_world.particles.find(x => x.id == id);
       if (!last_pos) continue;
       const angle = Math.atan2(y - last_pos.y, x - last_pos.x);
 
-      draw_tile(TILE_SPEAR, x, y, size, angle + Math.PI/2);
+      draw_tile(TILE_SPEAR, x, y, TILE_SIZE, angle + Math.PI/2);
 
       /* bad things happen if you forget to reset this */
       ctx.globalAlpha = 1.0;
